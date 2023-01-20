43a2081ff24d1e0bfc768957d2daa394320c5878
lcyoong <lcyoong@LCs-MacBook-Pro.local>
2023-01-20 10:43:04 +0800

    Setup Inertia, Ziggy, Tailwind, Vue, Heroicons and sample welcome page

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+const path = require('path');
 
 export default defineConfig({
     plugins: [
+        vue(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            '@' : path.resolve(__dirname, './resources/js'),
+            'ziggy': '/vendor/tightenco/ziggy/src/js',
+            'ziggy-vue': '/vendor/tightenco/ziggy/src/js/vue',
+        },
+    },    
 });
