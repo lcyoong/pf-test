2aefba8411435a793f813827d4d590e1ba0b62ba
lcyoong <lcyoong@LCs-MacBook-Pro.local>
2023-01-20 15:26:45 +0800

    Fix seeder to include send_to data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         User::factory(50)
-                ->has(Notification::factory()->count(3))
+                ->has(Notification::factory()->count(3)->state(function (array $attributes, User $user) {
+                    return ['send_to' => serialize([$user->id])];
+                }))
                 ->create();
     }
 }
